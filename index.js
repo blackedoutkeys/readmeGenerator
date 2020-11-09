3e30acaf1f81e001585ef81573893eb9964f6778
Isaac Frishman <ifrishman89@gmail.com>
2020-11-09 16:05:22 -0600

    files updated with JS and objects added to main js file

--- index.js
+++ index.js
@@ -1,7 +1,62 @@
-// array of questions for user
-const questions = [
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMD = require('./utils/generateMarkdown');
+const axios = require('axios');
 
+
+// array of questions for user
+const questions = [{
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please enter a brief description of your project"
+    },
+    {
+        type: "list",
+        name: "badge",
+        message: "Choose from the following badges",
+        choices: ['MIT', 'Apache', 'IBM', 'Perl']
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Are there any installation instructions required for this project to function?"
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "Please list any contribution guidelines"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Please list any test instructions"
+    },
+    {
+        type: "input",
+        name: "license",
+        message: "Please provide a license for the application.",
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Please enter your Github username to link"
+    },
+    {
+        type: "input",
+        name: "Questions or comments",
+        message: "Please provide any and all questions or concerns for support"
+    },
 ];
+
+//Inquirer to generate user questions
+inquirer.prompt(questions).then((answers) => {
+    console.log(JSON.stringify(answers, null, ' '));
+});
 
 // function to write README file
 function writeToFile(fileName, data) {
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,41 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(data, githubProfile) {
+  return `
+  
+  ## *${data.title}*
+  
+  ${data.badge ==="Apache" ? "## Apache <br> [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)" : data.badge ==="MIT" ? " ## MIT <br> [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)" : data.badge==="IBM" ? " ## IBM <br> [![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)" : " ## Perl <br> [![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)"}
+  
+  ## Table of Contents
 
+  -[Description](#Description) <br>
+
+  -[Installation](#Installation) <br>
+
+  -[Contributions](#Contributions) <br>
+
+  -[Test](#Test) <br>
+
+  -[GitHub](#Github) <br>
+
+  ## Description
+  ${data.description}
+
+  ## Installation
+  ${data.install}
+
+  ## Contributions
+  ${data.contribution}
+
+  ## Test
+  ${data.test}
+
+  ## GitHub
+  Username: ${gitHubProfile.name} <br>
+
+  Profile Link below <br>
+  -${gitHubProfile.profile}
 `;
-}
 
+}
 module.exports = generateMarkdown;
